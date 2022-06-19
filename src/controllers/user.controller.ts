2f742066cb084b2f7864fa09905074a48fc76a1f
Alieu Ndimbalane <alieundimbalane@gmail.com>
2022-06-19 21:22:30 +0000

    input validation

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,18 +1,26 @@
 import { Request, Response } from "express";
 import { getRepository } from "typeorm";
+import Joi from "joi";
 
 import { User } from "../entities/User";
 
 export const register = async (req: Request, res: Response) => {
-  const userRepository = getRepository(User);
-
-  if (Object.keys(req.body).length < 1) {
-    return res.status(422).json({
-      message: "Some data fields are missing",
-    });
-  }
+  const schema = Joi.object({
+    firstName: Joi.string().alphanum().min(3).max(10).required(),
+    lastName: Joi.string().alphanum().min(3).max(10).required(),
+    age: Joi.number().required(),
+    bloodGroup: Joi.string().min(1).max(3).required(),
+    address: Joi.string().alphanum().min(5).max(100).required(),
+    location: Joi.string().alphanum().required(),
+    phone: Joi.number().required(),
+    activeDonor: Joi.boolean().optional(),
+  });
 
   try {
+    const value = await schema.validateAsync(req.body);
+    const userRepository = getRepository(User);
+
+    try {
       const user = await userRepository.findOne({
         where: {
           phone: req.body.phone,
@@ -40,6 +48,11 @@ export const register = async (req: Request, res: Response) => {
         });
       }
     } catch (err) {}
+  } catch (err) {
+    return res.status(422).json({
+      error: err,
+    });
+  }
 
   return res.status(201).json({
     message: "User created",
@@ -47,24 +60,35 @@ export const register = async (req: Request, res: Response) => {
 };
 
 export const getUser = async (req: Request, res: Response) => {
-  const userRepository = getRepository(User);
-
-  if (!req.params.phone) {
-    return res.status(500).json({
-      error: "You must pass phone number",
-    });
-  }
-  const user = await userRepository.findOne({
-    where: {
-      phone: parseInt(req.params.phone),
-    },
+  const schema = Joi.object({
+    phone: Joi.number().required(),
   });
 
-  if (user) {
-    return res.status(200).json({
-      message: {
-        user,
+  try {
+    const value = await schema.validateAsync(req.params);
+    const userRepository = getRepository(User);
+
+    if (!req.params.phone) {
+      return res.status(500).json({
+        error: "You must pass phone number",
+      });
+    }
+    const user = await userRepository.findOne({
+      where: {
+        phone: parseInt(req.params.phone),
       },
+    });
+
+    if (user) {
+      return res.status(200).json({
+        message: {
+          user,
+        },
+      });
+    }
+  } catch (err) {
+    return res.status(422).json({
+      error: err,
     });
   }
   res.status(404).json({
@@ -72,62 +96,84 @@ export const getUser = async (req: Request, res: Response) => {
   });
 };
 
-export const registerPhone = async (req:Request, res: Response) => {
+export const registerPhone = async (req: Request, res: Response) => {
+  const schema = Joi.object({
+    phone: Joi.number().required(),
+  });
+
+  try {
+    const value = await schema.validateAsync(req.body);
     const userRepository = getRepository(User);
-    if(!req.body.phone){
-        return res.status(422).json({
-            error:"You have to pass phone number"
-        })
+    if (!req.body.phone) {
+      return res.status(422).json({
+        error: "You have to pass phone number",
+      });
     }
-        try {
-            const userStatus = await userRepository.findOne({
-                where: {
-                    phone: req.body.phone,
-                },
-            });
+    try {
+      const userStatus = await userRepository.findOne({
+        where: {
+          phone: req.body.phone,
+        },
+      });
 
-            if (userStatus) {
-                return res.status(200).json({
-                    message: "Phone number already exists",
-                });
-            }
+      if (userStatus) {
+        return res.status(200).json({
+          message: "Phone number already exists",
+        });
+      }
 
-            const user = userRepository.create({
-                firstName: "",
-                lastName: "",
-                age: 0,
-                bloodGroup: "",
-                activeDonor: false,
-                address: "",
-                location: "",
-                phone: req.body.phone,
-                verified: false,
-            });
-            const results = await userRepository.save(user);
-            console.log(results);
-        } catch (err) {
-            console.error(err);
-            return res.status(500).json({
-                error: "Server error 1",
-            });
-        }
-}
+      const user = userRepository.create({
+        firstName: "",
+        lastName: "",
+        age: 0,
+        bloodGroup: "",
+        activeDonor: false,
+        address: "",
+        location: "",
+        phone: req.body.phone,
+        verified: false,
+      });
+      const results = await userRepository.save(user);
+      console.log(results);
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({
+        error: "Server error 1",
+      });
+    }
+  } catch (err) {
+    return res.status(422).json({
+      error: err,
+    });
+  }
+};
 
 export const verifyPhone = async (req: Request, res: Response) => {
-	const pincode = req.body.code;
-	const userRepository = getRepository(User);
+  const schema = Joi.object({
+    code: Joi.number().required(),
+  });
 
-	const user = userRepository.findOne({
-		where:{
-			phone: parseInt(pincode)
-		},
-	})
+  try {
+    const value = await schema.validateAsync(req.body);
+    const pincode = req.body.code;
+    const userRepository = getRepository(User);
 
-	res.status(200).json({
-		message: "Phone number successfully verified"
-	})
+    const user = userRepository.findOne({
+      where: {
+        phone: parseInt(pincode),
+      },
+    });
 
-	res.status(401).json({
-		error: "Phone verification error"
-	})
-}
+    res.status(200).json({
+      message: "Phone number successfully verified",
+    });
+  } catch (err) {
+    return res.status(422).json({
+      error: err,
+    });
+  }
+
+  res.status(401).json({
+    error: "Phone verification error",
+  });
+};
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,7 +3,7 @@ const router = Router();
 
 import { register, verifyPhone, registerPhone, getUser } from "../controllers/user.controller";
 
-router.get("/", getUser);
+router.get("/:phone", getUser);
 router.post("/register/phone", registerPhone);
 router.post("/register", register);
 router.post("/verifyphone", verifyPhone);
